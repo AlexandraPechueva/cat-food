4c1138fc9ba0c43c3edd393a8ae6f324b121e60b
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-09-13 13:52:25 +0300

    Added ie11 support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(
   <React.StrictMode>
