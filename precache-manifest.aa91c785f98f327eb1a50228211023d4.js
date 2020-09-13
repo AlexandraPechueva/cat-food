87b3aaf7b2a29c2b425c6a0dde5deddd91756d82
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-09-13 16:25:24 +0300

    Updates

---
+++ precache-manifest.aa91c785f98f327eb1a50228211023d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bd08ceb308b65c28aabe8adb9d1fcf",
+    "url": "/cat-food/index.html"
+  },
+  {
+    "revision": "590c68e2668264d04634",
+    "url": "/cat-food/static/css/main.25d15fa4.chunk.css"
+  },
+  {
+    "revision": "a4ccb7da22247c90c54f",
+    "url": "/cat-food/static/js/2.2e9b3b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-food/static/js/2.2e9b3b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590c68e2668264d04634",
+    "url": "/cat-food/static/js/main.28738f4d.chunk.js"
+  },
+  {
+    "revision": "04d12276cb23ac32ceb1",
+    "url": "/cat-food/static/js/runtime-main.c3a21e33.js"
+  },
+  {
+    "revision": "3f938ebf543f2c8c818b101dee981eff",
+    "url": "/cat-food/static/media/exo2.0-thin.3f938ebf.woff2"
+  },
+  {
+    "revision": "661b0fb6d993d96e00133a6a568339d8",
+    "url": "/cat-food/static/media/exo2.0-thin.661b0fb6.ttf"
+  },
+  {
+    "revision": "8a2453604a33aba9e626630b745e357d",
+    "url": "/cat-food/static/media/exo2.0-thin.8a245360.woff"
+  },
+  {
+    "revision": "eab2a5406b414c442f9fec94900f50a5",
+    "url": "/cat-food/static/media/exo2.0-thin.eab2a540.eot"
+  },
+  {
+    "revision": "fd6f77d5b2af5b7fb7db2ef19eaae7c7",
+    "url": "/cat-food/static/media/funbox_logo.fd6f77d5.png"
+  }
+]);
